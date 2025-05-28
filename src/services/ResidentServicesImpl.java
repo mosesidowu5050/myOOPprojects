099a7132538e76a5b372cabc4d98708ef54de238
Mosess Idowu <dynamopraize5050@gmail.com>
2025-05-28 15:21:52 +0100

    update gate pass project

--- src/services/ResidentServicesImpl.java
+++ src/services/ResidentServicesImpl.java
@@ -1,12 +1,17 @@
 package services;
 
 import data.model.AccessToken;
+import data.model.Resident;
+import data.model.Visitor;
 import data.repository.*;
 import dtos.request.LoginServiceRequest;
 import dtos.request.ResidentServicesRequest;
 import dtos.request.VisitorInformationRequest;
 import dtos.responses.LoginServiceResponse;
 import dtos.responses.ResidentServicesResponse;
+import utils.Mapper;
+
+import java.util.Optional;
 
 import static utils.Mapper.*;
 
@@ -25,19 +30,53 @@ public class ResidentServicesImpl implements ResidentServices {
 
     @Override
     public ResidentServicesResponse register(ResidentServicesRequest residentServicesRequest) {
-        return map(residentServicesRequest);
+        verifyPhoneNumber(residentServicesRequest);
+        Resident resident = Mapper.accessTokenMapper(residentServicesRequest);
+        Resident savedResident = residentRepository.save(resident);
+
+        return response(savedResident);
     }
 
     @Override
-    public LoginServiceResponse login(LoginServiceRequest request) {
-        return loginMap(request);
+    public LoginServiceResponse login(LoginServiceRequest loginServiceRequest) {
+        validateResidentIDAndPhoneNumber(loginServiceRequest);
+        return Mapper.MapResponse();
     }
 
     @Override
     public AccessToken inviteVisitorAndGenerateToken(Long residentId, VisitorInformationRequest visitorRequest) {
-        return map(residentId, visitorRequest);
+        Resident resident = validateResidentId(residentId);
+        Visitor visitor = visitorMapper(visitorRequest);
+        Visitor visitorSaved = visitorRepository.save(visitor);
+
+        AccessToken token = accessTokenService.generateAccessToken(visitorSaved);
+        AccessToken savedToken = mapAccessToken(token);
+        savedToken.setResident(resident);
+        savedToken.setVisitor(visitor);
+
+        return savedToken;
     }
 
+    private void verifyPhoneNumber(ResidentServicesRequest residentServicesRequest) {
+        boolean phoneNumberExist = residentRepository.confirmPhoneNumber(residentServicesRequest.getPhoneNumber());
+        if (phoneNumberExist) throw new IllegalArgumentException("Phone number already exist");
+    }
+
+
+    private void validateResidentIDAndPhoneNumber(LoginServiceRequest loginServiceRequest) {
+        Optional<Resident> confirmResidentID = residentRepository.findById(loginServiceRequest.getId());
+        Resident confirmResidentPhoneNumber = residentRepository.findResidentByPhoneNumber(loginServiceRequest.getPhoneNumber());
+
+        if (confirmResidentID.isEmpty() || confirmResidentPhoneNumber == null) {
+            throw new RuntimeException("Invalid Id or Phone Number");
+        }
+    }
+
+
+    private Resident validateResidentId(Long residentId) {
+        return residentRepository.findById(residentId)
+                .orElseThrow(() -> new IllegalArgumentException("Resident not found"));
+    }
 
 
 }
--- src/utils/Mapper.java
+++ src/utils/Mapper.java
@@ -12,76 +12,46 @@ import dtos.responses.ResidentServicesResponse;
 import services.AccessTokenImpl;
 import services.AccessTokenService;
 
-import java.util.Optional;
-
 public class Mapper {
 
-    private static ResidentRepository residentRepository = new Residents();
-    private static VisitorRepository visitorRepository = new Visitors();
-    private static AccessTokenService accessTokenService = new AccessTokenImpl();
 
-    public static ResidentServicesResponse map(ResidentServicesRequest request) {
+    public static Resident accessTokenMapper(ResidentServicesRequest request) {
         Resident resident = new Resident();
         resident.setFullName(request.getFullName());
         resident.setHomeAddress(request.getHomeAddress());
         resident.setPhoneNumber(request.getPhoneNumber());
 
-        verifyPhoneNumber(request);
-        return response(resident);
+        return resident;
     }
 
-    private static ResidentServicesResponse response (Resident resident) {
-        Resident registerResident = residentRepository.save(resident);
-
+    public static ResidentServicesResponse response (Resident resident) {
         ResidentServicesResponse response = new ResidentServicesResponse();
-        response.setId(registerResident.getId());
-        response.setFullName(registerResident.getFullName());
-        response.setPhoneNumber(registerResident.getPhoneNumber());
+        response.setId(resident.getId());
+        response.setFullName(resident.getFullName());
+        response.setPhoneNumber(resident.getPhoneNumber());
 
         return response;
     }
 
-
-    public static LoginServiceResponse loginMap(LoginServiceRequest loginServiceRequest) {
-        Optional<Resident>confirmResidentID = residentRepository.findById(loginServiceRequest.getId());
-        Resident confirmResidentPhoneNumber = residentRepository.findResidentByPhoneNumber(loginServiceRequest.getPhoneNumber());
-
-        if (confirmResidentID.isEmpty() || confirmResidentPhoneNumber == null) {
-            throw new RuntimeException("Invalid Id or Phone Number");
-        }
+    public static LoginServiceResponse MapResponse () {
         LoginServiceResponse response = new LoginServiceResponse();
         response.setMessage("Login successful");
         return response;
     }
 
-
-    public static AccessToken map(Long residentId, VisitorInformationRequest visitorRequest) {
-        Resident resident = validateResidentId(residentId);
-
+    public static Visitor visitorMapper(VisitorInformationRequest visitorRequest) {
         Visitor visitor = new Visitor();
         visitor.setFullName(visitorRequest.getFullName());
         visitor.setPhoneNumber(visitorRequest.getPhoneNumber());
         visitor.setHomeAddress(visitorRequest.getHomeAddress());
-        visitorRepository.save(visitor);
 
-        AccessToken token = accessTokenService.generateAccessToken(visitor);
-        token.setResident(resident);
-        token.setVisitor(visitor);
+        return visitor;
+    }
+
+    public static AccessToken mapAccessToken(AccessToken token) {
         token.setOtpCode(token.getOtpCode());
         token.setOtpCreatedOn(token.getOtpCreatedOn());
-        token.setOtpExpiredDate(visitorRequest.getOtpExpiryDate());
 
         return token;
     }
-
-    private static Resident validateResidentId(Long residentId) {
-        return residentRepository.findById(residentId)
-               .orElseThrow(() -> new IllegalArgumentException("Resident not found"));
-    }
-
-    private static void verifyPhoneNumber(ResidentServicesRequest residentServicesRequest) {
-        boolean phoneNumberExist = residentRepository.confirmPhoneNumber(residentServicesRequest.getPhoneNumber());
-        if (phoneNumberExist) throw new IllegalArgumentException("Phone number already exist");
-    }
-
 }
